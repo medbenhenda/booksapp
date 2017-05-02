48d9ccdbff059e937a2fe95082b950eabe8e4d06
Mohamed Ben Henda <mohamed@viasyst.com>
2017-05-02 15:10:05 +0100

    create reducers, actions and containers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'javascript: The Good Parts', author: 'med', pages: 105 },
+        { title: 'Harry Potter!!!' , author: 'oli', pages: 65},
+        { title: 'The Dark Tower', author: 'ali', pages: 332 },
+        { title: 'Eloquent Ruby', author: 'edi', pages: 78 },
+    ]
+}
